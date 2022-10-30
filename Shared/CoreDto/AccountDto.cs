8b2ed67f6e3b85152a47d431e9953f173c1cf4de
bornbygoogle <bornbygoogle@yahoo.com>
2022-10-30 15:23:19 +0100

    add positive percentage

--- Shared/CoreDto/AccountDto.cs
+++ Shared/CoreDto/AccountDto.cs
@@ -2,26 +2,28 @@ using System.Collections.Generic;
 
 namespace BlazorApp.Shared.CoreDto
 {
-  public class AccountDto : DtoBase
-  {
-    public string AccountHolder { get; set; } = string.Empty;
-    public string AccountType { get; set; } = string.Empty;
-    public string Interval { get; set; } = string.Empty;
-    public int NbrSymbols { get; set; } = 0;
-    public string UseTDCountDown { get; set; } = string.Empty;
-    public string UseTDCombo { get; set; } = string.Empty;
-    public string UseConservatoire { get; set; } = string.Empty;
+    public class AccountDto : DtoBase
+    {
+        public string AccountHolder { get; set; } = string.Empty;
+        public string AccountType { get; set; } = string.Empty;
+        public string Interval { get; set; } = string.Empty;
+        public int NbrSymbols { get; set; } = 0;
+        public string UseTDCountDown { get; set; } = string.Empty;
+        public string UseTDCombo { get; set; } = string.Empty;
+        public string UseConservatoire { get; set; } = string.Empty;
 
-    /// <summary>
-    /// 0 = None / 1 = Buy only / 2 = Sell only / 3 = Both
-    /// </summary>
-    public string AllowBuySell { get; set; } = string.Empty;
+        /// <summary>
+        /// 0 = None / 1 = Buy only / 2 = Sell only / 3 = Both
+        /// </summary>
+        public string AllowBuySell { get; set; } = string.Empty;
 
-    public List<string> ListQuotes { get; set; } = new List<string>();
+        public List<string> ListQuotes { get; set; } = new List<string>();
 
-    public List<FinancialSummaryDto> RealAccountProfit { get; set; } = new List<FinancialSummaryDto>();
-    public List<FinancialSummaryDto> SimulatedAccountProfit { get; set; } = new List<FinancialSummaryDto>();
+        public List<FinancialSummaryDto> RealAccountProfit { get; set; } = new List<FinancialSummaryDto>();
+        public decimal? RealPositivePercentage { get; set; } = null;
+        public List<FinancialSummaryDto> SimulatedAccountProfit { get; set; } = new List<FinancialSummaryDto>();
+        public decimal? SimulatedPositivePercentage { get; set; } = null;
 
-    public AccountDto() { }
-  }
+        public AccountDto() { }
+    }
 }
--- Shared/CoreDto/FinancialSummaryDto.cs
+++ Shared/CoreDto/FinancialSummaryDto.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 
 namespace BlazorApp.Shared.CoreDto
 {
-  public class FinancialSummaryDto
-  {
-    public DateTime? Month { get; set; } = null;
+    public class FinancialSummaryDto
+    {
+        public DateTime? Month { get; set; } = null;
 
-    public int? CompletedTrades { get; set; } = null;
-    public int? CompletedTradesPositives { get; set; } = null;
-    public decimal? TotalBoughtCompletedTrades { get; set; } = null;
-    public decimal? TotalSoldCompletedTrades { get; set; } = null;
+        public int? CompletedTrades { get; set; } = null;
+        public int? CompletedTradesPositives { get; set; } = null;
+        public decimal? TotalBoughtCompletedTrades { get; set; } = null;
+        public decimal? TotalSoldCompletedTrades { get; set; } = null;
 
-    public int? IncompletedTrades { get; set; } = null;
-    public decimal? TotalBoughtIncompletedTrades { get; set; } = null;
-    public decimal? TotalSoldIncompletedTrades { get; set; } = null;
+        public int? IncompletedTrades { get; set; } = null;
+        public decimal? TotalBoughtIncompletedTrades { get; set; } = null;
+        public decimal? TotalSoldIncompletedTrades { get; set; } = null;
 
-    //quote - bought - sold (Completed)
-    public Dictionary<string, decimal> CompletedDetailsBought { get; set; } = new Dictionary<string, decimal>();
-    public Dictionary<string, decimal> CompletedDetailsSold { get; set; } = new Dictionary<string, decimal>();
+        //quote - bought - sold (Completed)
+        public Dictionary<string, decimal> CompletedDetailsBought { get; set; } = new Dictionary<string, decimal>();
+        public Dictionary<string, decimal> CompletedDetailsSold { get; set; } = new Dictionary<string, decimal>();
 
-    //quote - bought - sold (Incompleted)
-    public Dictionary<string, decimal> IncompletedDetailsBought { get; set; } = new Dictionary<string, decimal>();
-    public Dictionary<string, decimal> IncompletedDetailsSold { get; set; } = new Dictionary<string, decimal>();
+        //quote - bought - sold (Incompleted)
+        public Dictionary<string, decimal> IncompletedDetailsBought { get; set; } = new Dictionary<string, decimal>();
+        public Dictionary<string, decimal> IncompletedDetailsSold { get; set; } = new Dictionary<string, decimal>();
 
-    public FinancialSummaryDto() { }
-  }
+        public FinancialSummaryDto() { }
+    }
 }
