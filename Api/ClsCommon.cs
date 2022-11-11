9b0e30340032554b3173f366019913dcdf308cd0
bornbygoogle <bornbygoogle@yahoo.com>
2022-11-11 22:37:50 +0100

    Change url_server

--- Api/ClsCommon.cs
+++ Api/ClsCommon.cs
@@ -1,17 +1,15 @@
 ﻿using BlazorApp.Shared.CoreDto;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlazorApp.Api
 {
     public class ClsCommon
     {
-        public static string URL_SERVER = "https://logservice-app-20221012212833.redmushroom-aa658d30.northeurope.azurecontainerapps.io";
+        public static string URL_SERVER = "https://logservice-app-20221111153833.thankfuldesert-bb5e8557.northeurope.azurecontainerapps.io";
+        public static string URL_SERVER_2 = "https://logservice-app-20221012212833.redmushroom-aa658d30.northeurope.azurecontainerapps.io";
         //public static string URL_SERVER = "https://localhost:7132";
 
         public static List<LogInfoItemDto> GetLogs(string sMethod, string accType, string accHolder, string symbol = null)
